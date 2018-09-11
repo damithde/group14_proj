2403f38df2f6dd0b986904aeceb7e599d22a78c3
Dinuka Kasun Medis <dinuka-kasun-medis@users.noreply.github.com>
2018-09-11 21:14:07 +0530

    edited

---
+++ School system/header.php
@@ -0,0 +1,17 @@
+<body class="skin-blue">
+<div class="wrapper">
+
+    <header class="main-header">
+        <!-- Logo -->
+        <a href="" class="logo"><b>Admin</b></a>
+        <!-- Header Navbar: style can be found in header.less -->
+        <nav class="navbar navbar-static-top" role="navigation">
+            <!-- Sidebar toggle button-->
+            <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+                <span class="sr-only">Toggle navigation</span>
+            </a>
+            <div class="navbar-custom-menu">
+
+            </div>
+        </nav>
+    </header>
