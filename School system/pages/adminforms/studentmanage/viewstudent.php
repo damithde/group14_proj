360af4c7ba92bdda66b5f838c71822816da26efa
Dinuka Kasun Medis <dinuka-kasun-medis@users.noreply.github.com>
2018-09-25 22:05:52 +0530

    admin
    calendar
    modify dashboard

--- School system/pages/adminforms/studentmanage/viewstudent.php
+++ School system/pages/adminforms/studentmanage/viewstudent.php
@@ -199,63 +199,62 @@
                             </div>
                     </form>
                 </div><!-- /.box -->
-
-<!--                Exam results-->
-                <div class="box box-primary">
-                        <div class="box-header with-border">
-                            <h3 class="box-title">Exam Results</h3>
-                        </div><!-- /.box-header -->
-                        <div class="box-body">
-                            <div class="table-responsive">
-                                <table class="table no-margin">
-                                    <thead>
-                                    <tr>
-                                        <th>Exam Name</th>
-                                        <th>Date</th>
-                                        <th>Subject</th>
-                                        <th>Grade Point</th>
-                                        <th>Precentage Point</th>
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                    <tr>
-                                        <td><a href="pages/examples/invoice.html">Math in class</a></td>
-                                        <td>2012/02/02</td>
-                                        <td>Math</td>
-                                        <td>4</td>
-                                        <td><span class="label label-warning">57</span></td>
-                                    </tr>
-                                    <tr>
-                                        <td><a href="pages/examples/invoice.html">Math in class</a></td>
-                                        <td>2012/02/02</td>
-                                        <td>Math</td>
-                                        <td>4</td>
-                                        <td><span class="label label-danger">10</span></td>
-                                    </tr>
-                                    <tr>
-                                        <td><a href="pages/examples/invoice.html">Math in class</a></td>
-                                        <td>2012/02/02</td>
-                                        <td>Math</td>
-                                        <td>4</td>
-                                        <td><span class="label label-info">70</span></td>
-                                    </tr>
-                                    <tr>
-                                        <td><a href="pages/examples/invoice.html">Math in class</a></td>
-                                        <td>2012/02/02</td>
-                                        <td>Math</td>
-                                        <td>4</td>
-                                        <td><span class="label label-success">85</span></td>
-                                    </tr>
-                                    </tbody>
-                                </table>
-                            </div><!-- /.table-responsive -->
-                        </div><!-- /.box-body -->
-                        <div class="box-footer clearfix">
-                            <a href="javascript::;" class="btn btn-sm btn-default btn-flat pull-right">View All Exams</a>
-                        </div><!-- /.box-footer -->
-                    </div><!-- /.box -->
             </div>
 
+            <!--                Exam results-->
+            <div class="box box-primary">
+                <div class="box-header with-border">
+                    <h3 class="box-title">Exam Results</h3>
+                </div><!-- /.box-header -->
+                <div class="box-body">
+                    <div class="table-responsive">
+                        <table class="table no-margin">
+                            <thead>
+                            <tr>
+                                <th>Exam Name</th>
+                                <th>Date</th>
+                                <th>Subject</th>
+                                <th>Grade Point</th>
+                                <th>Precentage Point</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td><a href="pages/examples/invoice.html">Math in class</a></td>
+                                <td>2012/02/02</td>
+                                <td>Math</td>
+                                <td>4</td>
+                                <td><span class="label label-warning">57</span></td>
+                            </tr>
+                            <tr>
+                                <td><a href="pages/examples/invoice.html">Math in class</a></td>
+                                <td>2012/02/02</td>
+                                <td>Math</td>
+                                <td>4</td>
+                                <td><span class="label label-danger">10</span></td>
+                            </tr>
+                            <tr>
+                                <td><a href="pages/examples/invoice.html">Math in class</a></td>
+                                <td>2012/02/02</td>
+                                <td>Math</td>
+                                <td>4</td>
+                                <td><span class="label label-info">70</span></td>
+                            </tr>
+                            <tr>
+                                <td><a href="pages/examples/invoice.html">Math in class</a></td>
+                                <td>2012/02/02</td>
+                                <td>Math</td>
+                                <td>4</td>
+                                <td><span class="label label-success">85</span></td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div><!-- /.table-responsive -->
+                </div><!-- /.box-body -->
+                <div class="box-footer clearfix">
+                    <a href="javascript::;" class="btn btn-sm btn-default btn-flat pull-right">View All Exams</a>
+                </div><!-- /.box-footer -->
+            </div><!-- /.box -->
 
 
         </div>
