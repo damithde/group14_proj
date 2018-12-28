25ee18cbcb864a3955cd539a7c643c09fdf87a27
Dinuka Kasun Medis <dinuka-kasun-medis@users.noreply.github.com>
2018-12-28 15:51:36 +0530

    registre teacher
    preview panel

--- School system/pages/adminforms/teachermanage/registerteacher.php
+++ School system/pages/adminforms/teachermanage/registerteacher.php
@@ -23,16 +23,16 @@
                         <div class="box-body">
                             <div class="form-group">
                                 <label for="exampleInputEmail1">Name:</label>
-                                <input type="email" class="form-control" id="studentNameTxt" placeholder="Enter Name">
+                                <input  onkeyup="prvName()"  class="form-control" id="teacherNameTxt" placeholder="Enter Name">
                             </div>
                             <div class="form-group">
                                 <label >Gender:</label>
                                 <div class="row">
                                     <div class="col-md-4">
-                                        <input type="radio"  name="parent"  id="father" class="form-check-input" value="Father" > Male
+                                        <input onkeyup="prvGender()" type="radio"  name="parent"  id="father" class="form-check-input" value="Father" > Male
                                     </div>
                                     <div class="col-md-4">
-                                        <input type="radio"  name="parent" id="mother" class="form-check-input" value="Mother">  Female
+                                        <input onkeyup="prvGender()" type="radio"  name="parent" id="mother" class="form-check-input" value="Mother">  Female
                                     </div>
                                 </div>
                             </div>
@@ -46,28 +46,16 @@
                                 </div><!-- /.input group -->
                             </div>
                             <div class="form-group">
-                                <label for="exampleInputEmail1">Religion:</label>
-                                <input type="email" class="form-control" id="studentNameTxt" placeholder="Enter Religion">
-                            </div>
-                            <div class="form-group">
                                 <label for="exampleInputEmail1">Email:</label>
-                                <input type="email" class="form-control" id="addmissionNoTxt" placeholder="Enter Admission No">
+                                <input onkeyup="prvEmail()"  class="form-control" id="teacherEmailText" placeholder="Enter Email">
                             </div>
                             <div class="form-group">
-                                <label for="exampleInputEmail1">Admission No:</label>
-                                <input type="email" class="form-control" id="studentNameTxt" placeholder="Enter Nationality">
-                            </div>
-                            <div class="form-group">
-                                <label for="exampleInputEmail1">Class:</label>
-                                <input type="email" class="form-control" id="studentNameTxt" placeholder="Enter Nationality">
-                            </div>
-                            <div class="form-group">
-                                <label for="exampleInputEmail1">Section:</label>
-                                <input type="email" class="form-control" id="studentNameTxt" placeholder="Enter Nationality">
+                                <label for="exampleInputEmail1">Registration No:</label>
+                                <input onkeyup="prvRegNo()" type="email" class="form-control" id="teacherRegNoTxt" placeholder="Enter Registration No">
                             </div>
                             <div class="form-group">
                                 <label for="exampleInputEmail1">Address:</label>
-                                <textarea class="form-control" id="addressTxt" rows="3" placeholder="Enter Address"></textarea>
+                                <textarea onkeyup="prvaddress()" class="form-control" id="teacherAddressTxt" rows="3" placeholder="Enter Address"></textarea>
                             </div>
                             <div class="form-group">
                                 <label>Phone No:</label>
@@ -75,7 +63,7 @@
                                     <div class="input-group-addon">
                                         <i class="fa fa-phone"></i>
                                     </div>
-                                    <input type="text" class="form-control" data-inputmask='"mask": "(999) 9999999"' data-mask/>
+                                    <input onkeyup="prvPhoneNo()" type="text" id="teacherTelNoTxt" class="form-control" data-inputmask='"mask": "(999) 9999999"' data-mask/>
                                 </div><!-- /.input group -->
                             </div>
                             <div class="form-group">
@@ -92,7 +80,7 @@
             </div>
 
             <div class="col-md-6">
-                <!--                student details-->
+                <!--                teacher details-->
                 <div class="box box-primary">
                     <div class="box-header with-border">
                         <h3 class="box-title">Preview</h3>
@@ -119,21 +107,7 @@
                                 <div class="col-md-6">
                                     <div class="row">
                                         <div class="col-md-3">
-                                            <!-- Profile picture -->
-                                            <!-- <div >
-                                                <img src="../../../dist/img/user_100x128.png" alt="User Image">
-                                            </div>
-                                            <div class="form-group">
-                                                <div class="col-md-2">
-                                                    <label><a href="../teachermanage/registerteacher.php"><i class="fa fa-edit"></i></a></label>
-                                                </div>
-                                                <div class="col-md-2">
-                                                    <label><a href="../teachermanage/registerteacher.php"><i class="fa  fa-print"></i></a></label>
-                                                </div>
-                                                <div class="col-md-2">
-                                                    <label><a href="../teachermanage/registerteacher.php"><i class="fa fa-download"></i></a></label>
-                                                </div>
-                                            </div> -->
+                                            <!--  -->
                                         </div>
                                         <div class="col-md-9">
                                             <!--Lable names-->
@@ -147,19 +121,10 @@
                                                 <label class="text-muted" for="exampleInputEmail1">Date Of Birth:</label>
                                             </div>
                                             <div class="form-group">
-                                                <label class="text-muted" for="exampleInputEmail1">Religion:</label>
-                                            </div>
-                                            <div class="form-group">
                                                 <label class="text-muted" >Email:</label>
                                             </div>
                                             <div class="form-group">
-                                                <label class="text-muted" for="exampleInputEmail1">Admission Date:</label>
-                                            </div>
-                                            <div class="form-group">
-                                                <label class="text-muted" for="exampleInputEmail1">Class:</label>
-                                            </div>
-                                            <div class="form-group">
-                                                <label class="text-muted" for="exampleInputEmail1">Section:</label>
+                                                <label class="text-muted" for="exampleInputEmail1">Registration No:</label>
                                             </div>
                                             <div class="form-group">
                                                 <label class="text-muted" for="exampleInputEmail1">Address:</label>
@@ -173,34 +138,25 @@
 
                                 <div class="col-md-6">
                                     <div class="form-group">
-                                        <label for="exampleInputEmail1">Y.Dinuka Kasun Medis</label>
+                                        <label id="previewName">Y.Dinuka Kasun Medis</label>
                                     </div>
                                     <div class="form-group">
-                                        <label for="exampleInputEmail1">Male</label>
+                                        <label id="previewGender">Male</label>
                                     </div>
                                     <div class="form-group">
-                                        <label for="exampleInputEmail1">1995/09/20</label>
+                                        <label id="previewDOB">1995/09/20</label>
                                     </div>
                                     <div class="form-group">
-                                        <label>Srilankan</label>
+                                        <label id="previewEmail">dinuka.kasunds@gmail.com</label>
                                     </div>
                                     <div class="form-group">
-                                        <label for="exampleInputEmail1">dinuka.kasunds@gmail.com</label>
+                                        <label id="previewRegNo">2015/02/02</label>
                                     </div>
                                     <div class="form-group">
-                                        <label for="exampleInputEmail1">2015/02/02</label>
+                                        <label id="previewAddress">73,Dampe,Madapatha</label>
                                     </div>
                                     <div class="form-group">
-                                        <label for="exampleInputEmail1">m1</label>
-                                    </div>
-                                    <div class="form-group">
-                                        <label>12</label>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="exampleInputEmail1">73,Dampe,Madapatha</label>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="exampleInputEmail1">0717275722</label>
+                                        <label id="previewPhoneNo">0717275722</label>
                                     </div>
                                     <div >
                                         <button type="submit" class="btn btn-primary">Register</button>
@@ -219,4 +175,121 @@
         </div>
     </section>
 </section>
+<script>
+    var tchrName = document.getElementById("teacherNameTxt").value;
+    var tchrGend = document.querySelector('input[name=gender]:checked').value;
+    var tchrbd = document.querySelector('date').value;
+    var tchrReli = document.getElementById("teacherReligionTxt").value;
+    var tchrEmail = document.getElementById("teacherEmailTxt").value;
+    var tchrAdNo = document.getElementById("teacherAdmissionNoTxt").value;
+    var tchrClass = document.getElementById("teacherClassTxt").value;
+    var tchrGrade = document.getElementById("teacherGradeTxt").value;
+    var tchrAddress = document.getElementById("teacherAddressTxt").value;
+    var par = document.querySelector('input[name=parent]:checked').value;
+    var parName = document.getElementById("parentNameTxt").value;
+    var parOcupation = document.getElementById("parentOcupationTxt").value;
+    var parEmail = document.getElementById("parentEmail").value;
+    var parAddress = document.getElementById("parentAddressTxt").value;
+    var parent={name:parName,parent:par,occupation:parOcupation,email:parEmail};
+    var teacher= [tchrAdNo,tchrFName,tchrLName,contact,tchrbd,tchrAddress,tchrReli,school,tchrGrade,tchrClass,tchrEmail];
+    addteacher(teacher,parent);
+
+    // function prvUpdate() {
+    //     //var tchrName = document.getElementById("teacherNameTxt").value;
+    //     document.getElementById('previewName').innerHTML = tchrName;
+    // // alert("You pre   ssed a key inside the input field");
+    // }
+
+</script>
+<script>
+    function prvName() {
+        var tchrName = document.getElementById("teacherNameTxt").value;
+        document.getElementById('previewName').innerHTML = tchrName;
+    }
+</script>
+<script>
+    function prvGender() {
+        var tchrGender = document.getElementById("input[name=parent]:checked").value;
+        document.getElementById('previewGender').innerHTML = tchrGender;
+    }
+</script>
+<script>
+    function prvDOB() {
+        var tchrDOB = document.getElementById("teacherDOBTxt").value;
+        document.getElementById('previewDOB').innerHTML = tchrDOB;
+    }
+</script>
+<script>
+    function prvEmail() {
+        var tchrEmail = document.getElementById("teacherEmailText").value;
+        document.getElementById('previewEmail').innerHTML = tchrEmail;
+    }
+</script>
+<script>
+    function prvRegNo() {
+        var tchrReg = document.getElementById("teacherRegNoTxt").value;
+        document.getElementById('previewRegNo').innerHTML = tchrReg;
+    }
+</script>
+<script>
+    function prvAddress() {
+        var tchrAddress = document.getElementById("teacherAddressTxt").value;
+        document.getElementById('previewAddress').innerHTML = tchrAddress;
+    }
+</script>
+<script>
+    function prvPhoneNo() {
+        var tchrTel = document.getElementById("teacherTelNoTxt").value;
+        document.getElementById('previewPhoneNo').innerHTML = tchrTel;
+    }
+</script>
+
+
+
 <?php include_once('../admincommon/footer.php'); ?>
+
+
+<?php
+    $target_dir = "uploads/";
+    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+    // Check if image file is a actual image or fake image
+    if(isset($_POST["submit"])) {
+        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+        if($check !== false) {
+            echo "File is an image - " . $check["mime"] . ".";
+            $uploadOk = 1;
+        } else {
+            echo "File is not an image.";
+            $uploadOk = 0;
+        }
+    }
+    // Check if file already exists
+    if (file_exists($target_file)) {
+        echo "Sorry, file already exists.";
+        $uploadOk = 0;
+    }
+    // Check file size
+    if ($_FILES["fileToUpload"]["size"] > 500000) {
+        echo "Sorry, your file is too large.";
+        $uploadOk = 0;
+    }
+    // Allow certain file formats
+    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+    && $imageFileType != "gif" ) {
+        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+        $uploadOk = 0;
+    }
+    // Check if $uploadOk is set to 0 by an error
+    if ($uploadOk == 0) {
+        echo "Sorry, your file was not uploaded.";
+    // if everything is ok, try to upload file
+    } else {
+        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+            echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+        } else {
+            echo "Sorry, there was an error uploading your file.";
+        }
+    }
+?>
