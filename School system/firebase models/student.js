60d09b2c0cb96c7ac5464315eb7339150792984e
damithde <damithdesilva2@gmail.com>
2019-01-02 20:59:10 +0530

    promote1

--- School system/firebase models/student.js
+++ School system/firebase models/student.js
@@ -114,53 +114,32 @@ function updatestudent(regno,student,parent){
 
 }
 
-function promotestudent(student,id){
+function promotestudent(idlist,tograde,toclass){
+    idlist.forEach(function(stid){
+        db.collection("students").where("regno","==",stid)
+        .get()
+        .then(function(querySnapshot) {
+            querySnapshot.forEach(function(doc) {
+                db.collection("students").doc(doc.id).update({
+                    "grade": tograde,
+                    "class": toclass
+                })
+                .then(function() {
+                        console.log("Document successfully updated!");
+                    }).catch(function(error) {
+                        console.error("Error removing document: ", error);
+                    });      
+                    
+                    
+                });
 
-    db.collection("students").doc(id).update({
-        "grade": student[0],
-        "class": student[1]
-    })
-    .then(function() {
-        db.collection("schools").doc(student[8]).collection("classes").doc(student[10]).collection("students").doc(id).delete().then(function() {
-            console.log("Document successfully deleted!");
-        }).catch(function(error) {
-            console.error("Error removing document: ", error);
-        });      
-        
-        
-    });
-
-    db.collection("students").where("regno","==",id)
-    .get()
-    .then(function(querySnapshot) {
-        querySnapshot.forEach(function(doc) {
-            var st= doc.data();
-        });
-    })
-    .catch(function(error) {
-        console.log("Error getting documents: ", error);
-    });    
-    
-    db.collection("schools").doc(student[8]).collection("classes").doc(student[10]).collection("students").doc(student[0]).set({
-        regno: student[0],
-        Fname: student[1],
-        Lname: student[2],
-        country: student[3],
-        contact: student[4],
-        dob: student[5],
-        nationality: student[6],
-        religion: student[7],
-        school: student[8],
-        grade: student[9],
-        class: student[10],
-        parent: parent
-    })
-
-   // console.log("Document written with ID: ", docRef.id);
-.catch(function(error) {
-    console.error("Error adding document: ", error);
-});
+            });
+        })
+        .catch(function(error) {
+            console.log("Error getting documents: ", error);
+        });    
 
 }
+    
 
 
