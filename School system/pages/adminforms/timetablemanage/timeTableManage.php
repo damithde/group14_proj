38ca47daa12cae1ad3cf4f2d964dd345c0e40c2e
manjitha-teshara <teranteshata01@gmail.com>
2019-01-04 12:20:44 +0530

    Time table manage edit

---
+++ School system/pages/adminforms/timetablemanage/timeTableManage.php
@@ -0,0 +1,202 @@
+<?php include_once('../admincommon/head.php'); ?>
+<?php include_once('../admincommon/header.php'); ?>
+<?php include_once('../admincommon/sidebar.php'); ?>
+<?php include_once('../admincommon/script.php'); ?>
+
+<body class="skin-blue">
+<div class="wrapper">
+<section class="content-wrapper">
+    <section class="content-header">
+        <!--                Exam results-->
+        <div class="box box-primary">
+            <div class="box-header with-border">
+                <h3 class="box-title">Create Time Table</h3>
+            </div><!-- /.box-header -->
+            <div class="box-body">
+                <div class="table-responsive">
+                    <thead>
+                           <p>Time Table for</p> 
+                <label class="col-md-5">
+                        <input id="up_radio" type="radio" name="optionsRadios" value="class" checked="checked">
+                        Class
+                    </label>
+                    <label class="radio inline">
+                        <input id="ov_radio" type="radio" name="optionsRadios" value="teacher">  
+                        Teacher
+                    </label>
+                            
+                        </thead>
+                </div>
+                <div class="table-responsive">
+                   <form role="form" action="" method="POST">
+                       
+                        <div id="classDev" class="login-box-body" >
+                            <form class="">
+                                <label>Class No</label><input name="classNo"><label>Class Teacher</label><input name="classTeacher">
+                            </form>
+                       
+                       </div>
+                        <div id="teacherDev" class="login-box-body" style="display:none">
+                            <form class="">
+                                <label>Teacher Reg No</label><input name="teacherRegNo"><label>Teacher Name</label><input name="teacherName">
+                            </form>
+                            
+                       </div>
+                       
+                    <table class="table no-margin">
+                        
+                        
+                        <thead>
+                            <tr>
+                                <th>Time Perod</th>
+                                <th>Monday</th>
+                                <th>Tuesday</th>
+                                <th>Wendsday</th>
+                                <th>Thersday</th>
+                                <th>Friday</th>
+                            </tr>
+                        </thead>
+                        
+                            <p>test student</p>
+                        <tbody>
+                        <tr>
+                            <td><input name="time2" class="tableTxtEdit" value="07.30-07.50"></td>
+                            <td></td>
+                            <td></td>
+                            <td style="font-size:25px;font-weight: bold;" class="tableTxtEdit">Reminding Religion</td>
+                            <td></td>
+                            <td></td>
+                            
+                            
+                        </tr>
+                        <tr>
+                            <td><input name="timeView1" class="tableTxtEdit" value="07.50-08.30"></td>
+                            <td><input name="subjectView1" class="tableTxtEdit" value=""></td>
+                            <td><input name="subjectView2" class="tableTxtEdit" value=""></td>
+                            <td><input name="subjectView3" class="tableTxtEdit" value=""></td>
+                            <td><input name="subjectView4" class="tableTxtEdit" value=""></td>
+                            <td><input name="subjectView5" class="tableTxtEdit" value=""></td>
+                           
+                            
+                        </tr>
+                        <tr>
+                            <td><input name="timeView12" class="tableTxtEdit" value="08.30-09.10"></td>
+                            <td><input name="subjectView12" class="tableTxtEdit" value=""></td>
+                            <td><input name="subjectView22" class="tableTxtEdit" value=""></td>
+                            <td><input name="subjectView32" class="tableTxtEdit" value=""></td>
+                            <td><input name="subjectView42" class="tableTxtEdit" value=""></td>
+                            <td><input name="subjectView52" class="tableTxtEdit" value=""></td>
+                        </tr>
+                        <tr>
+                            <td><input name="timeView13" class="tableTxtEdit" value="09.10-09.50"></td>
+                            <td><input name="subjectView13" class="tableTxtEdit" value=""></td>
+                            <td><input name="subjectView23" class="tableTxtEdit" value=""></td>
+                            <td><input name="subjectView33" class="tableTxtEdit" value=""></td>
+                            <td><input name="subjectView43" class="tableTxtEdit" value=""></td>
+                            <td><input name="subjectView53" class="tableTxtEdit" value=""></td>
+                        </tr>
+                        <tr>
+                            <td><input name="timeView14" class="tableTxtEdit" value="09.50-10.30"></td>
+                            <td><input name="subjectView14" class="tableTxtEdit" value=""></td>
+                            <td><input name="subjectView24" class="tableTxtEdit" value=""></td>
+                            <td><input name="subjectView34" class="tableTxtEdit" value=""></td>
+                            <td><input name="subjectView44" class="tableTxtEdit" value=""></td>
+                            <td><input name="subjectView54" class="tableTxtEdit" value=""></td>
+                        </tr>
+                        <tr>
+                            <td><input name="time2interval" class="tableTxtEdit" value="10.30-10.50"></td>
+                            <td></td>
+                            <td></td>
+                            <td style="font-size:25px;font-weight: bold;" class="tableTxtEdit">Interval</td>
+                            <td></td>
+                            <td></td>
+                           
+                        </tr>
+                        <tr>
+                            <td><input name="timeView15" class="tableTxtEdit" value="10.50-11.30"></td>
+                            <td><input name="subjectView15" class="tableTxtEdit" value=""></td>
+                            <td><input name="subjectView25" class="tableTxtEdit" value=""></td>
+                            <td><input name="subjectView35" class="tableTxtEdit" value=""></td>
+                            <td><input name="subjectView45" class="tableTxtEdit" value=""></td>
+                            <td><input name="subjectView55" class="tableTxtEdit" value=""></td>
+                           
+                        </tr>
+                        <tr>
+                            <td><input name="timeView16" class="tableTxtEdit" value="11.30-12.10"></td>
+                            <td><input name="subjectView16" class="tableTxtEdit" value=""></td>
+                            <td><input name="subjectView26" class="tableTxtEdit" value=""></td>
+                            <td><input name="subjectView36" class="tableTxtEdit" value=""></td>
+                            <td><input name="subjectView46" class="tableTxtEdit" value=""></td>
+                            <td><input name="subjectView56" class="tableTxtEdit" value=""></td>
+                           
+                        </tr>
+                            
+                        <tr>
+                            <td><input name="timeView17" class="tableTxtEdit" value="12.10-12.50"></td>
+                            <td><input name="subjectView17" class="tableTxtEdit" value=""></td>
+                            <td><input name="subjectView27" class="tableTxtEdit" value=""></td>
+                            <td><input name="subjectView37" class="tableTxtEdit" value=""></td>
+                            <td><input name="subjectView47" class="tableTxtEdit" value=""></td>
+                            <td><input name="subjectView57" class="tableTxtEdit" value=""></td>
+                           
+                        </tr>
+                        <tr>
+                            
+                            <td><input name="timeView18" class="tableTxtEdit" value="12.50-13.30"></td>
+                            <td><input name="subjectView18" class="tableTxtEdit" value=""></td>
+                            <td><input name="subjectView28" class="tableTxtEdit" value=""></td>
+                            <td><input name="subjectView38" class="tableTxtEdit" value=""></td>
+                            <td><input name="subjectView48" class="tableTxtEdit" value=""></td>
+                            <td><input name="subjectView58" class="tableTxtEdit" value=""></td>
+                               
+                        </tr>
+                            
+                        <tr><td></td>
+                            <td></td>
+                            <td></td>
+                            <td><button name="search" type="button" class="btn btn-primary">Search</button></td>
+                            <td><button name="add" type="button" class="btn btn-primary">Add</button></td>
+                            <td><button name="delete" type="button" class="btn btn-primary">Delete</button></td>
+                          
+                            </tr>
+                            
+                        </tbody>
+                        
+                    </table>
+                        
+                    </form>
+                </div><!-- /.table-responsive -->
+            </div><!-- /.box-body -->
+            <div class="box-footer clearfix">
+               
+            </div><!-- /.box-footer -->
+        </div><!-- /.box -->
+    </section>
+</section>
+</div>
+<?php include_once('../admincommon/footer.php'); ?>
+    
+    <script>
+
+	$('input[name="optionsRadios"]').on('change', function(){
+        if ($(this).val()=='class') {
+            
+            
+            document.getElementById("classDev").style.display = 'block';
+            document.getElementById("teacherDev").style.display = 'none';
+
+        } else  {
+        
+            // $("#cont").text("show Overwritten");
+            document.getElementById("classDev").style.display = 'none';
+            document.getElementById("teacherDev").style.display = 'block';
+        }
+    });
+    
+</script>
+    
+    </body>
+
+
+
+
