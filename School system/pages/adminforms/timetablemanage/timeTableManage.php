409c44f65ddafb84c638fc07b24924eadb0fbba8
manjitha-teshara <teranteshata01@gmail.com>
2019-01-06 17:58:43 +0530

    time table search function version 2

--- School system/pages/adminforms/timetablemanage/timeTableManage.php
+++ School system/pages/adminforms/timetablemanage/timeTableManage.php
@@ -479,56 +479,56 @@
         var clz = document.getElementById("classNo").value;   
         
         getClassTimeTable(clz);
-        var d1="monday";
-        getTimeSlot("07.50-08.30",d1,'subjectView1','teacherView1');//subjectVie
-        getTimeSlot("08.30-09.10",d1,'subjectView12','teacherView12');
-        getTimeSlot("09.10-09.50",d1,'subjectView13','teacherView13');
-        getTimeSlot("09.50-10.30",d1,'subjectView14','teacherView14');
-        getTimeSlot("10.50-11.30",d1,'subjectView15','teacherView15');
-        getTimeSlot("11.30-12.10",d1,'subjectView16','teacherView16');
-        getTimeSlot("12.10-12.50",d1,'subjectView17','teacherView17');
-        getTimeSlot("12.50-13.30",d1,'subjectView18','teacherView18');
-        
-        var d2="tuesday";
-        getTimeSlot("07.50-08.30",d2,'subjectView2','teacherView2');//subjectVie
-        getTimeSlot("08.30-09.10",d2,'subjectView22','teacherView22');
-        getTimeSlot("09.10-09.50",d2,'subjectView23','teacherView23');
-        getTimeSlot("09.50-10.30",d2,'subjectView24','teacherView24');
-        getTimeSlot("10.50-11.30",d2,'subjectView25','teacherView25');
-        getTimeSlot("11.30-12.10",d2,'subjectView26','teacherView26');
-        getTimeSlot("12.10-12.50",d2,'subjectView27','teacherView27');
-        getTimeSlot("12.50-13.30",d2,'subjectView28','teacherView28');
-        
-        var d3="wednesday";
-        getTimeSlot("07.50-08.30",d3,'subjectView3','teacherView3');//subjectVie
-        getTimeSlot("08.30-09.10",d3,'subjectView32','teacherView32');
-        getTimeSlot("09.10-09.50",d3,'subjectView33','teacherView33');
-        getTimeSlot("09.50-10.30",d3,'subjectView34','teacherView34');
-        getTimeSlot("10.50-11.30",d3,'subjectView35','teacherView35');
-        getTimeSlot("11.30-12.10",d3,'subjectView36','teacherView36');
-        getTimeSlot("12.10-12.50",d3,'subjectView37','teacherView37');
-        getTimeSlot("12.50-13.30",d3,'subjectView38','teacherView38');
-        
-        var d4="thursday";
-        getTimeSlot("07.50-08.30",d4,'subjectView4','teacherView4');//subjectVie
-        getTimeSlot("08.30-09.10",d4,'subjectView42','teacherView42');
-        getTimeSlot("09.10-09.50",d4,'subjectView43','teacherView43');
-        getTimeSlot("09.50-10.30",d4,'subjectView44','teacherView44');
-        getTimeSlot("10.50-11.30",d4,'subjectView45','teacherView45');
-        getTimeSlot("11.30-12.10",d4,'subjectView46','teacherView46');
-        getTimeSlot("12.10-12.50",d4,'subjectView47','teacherView47');
-        getTimeSlot("12.50-13.30",d4,'subjectView48','teacherView48');
-        
-        var d5="friday";
-        getTimeSlot("07.50-08.30",d5,'subjectView5','teacherView5');//subjectVie
-        getTimeSlot("08.30-09.10",d5,'subjectView52','teacherView52');
-        getTimeSlot("09.10-09.50",d5,'subjectView53','teacherView53');
-        getTimeSlot("09.50-10.30",d5,'subjectView54','teacherView54');
-        getTimeSlot("10.50-11.30",d5,'subjectView55','teacherView55');
-        getTimeSlot("11.30-12.10",d5,'subjectView56','teacherView56');
-        getTimeSlot("12.10-12.50",d5,'subjectView57','teacherView57');
-        getTimeSlot("12.50-13.30",d5,'subjectView58','teacherView58');
-        
+//        var d1="monday";
+//        getTimeSlot("07.50-08.30",d1,'subjectView1','teacherView1');//subjectVie
+//        getTimeSlot("08.30-09.10",d1,'subjectView12','teacherView12');
+//        getTimeSlot("09.10-09.50",d1,'subjectView13','teacherView13');
+//        getTimeSlot("09.50-10.30",d1,'subjectView14','teacherView14');
+//        getTimeSlot("10.50-11.30",d1,'subjectView15','teacherView15');
+//        getTimeSlot("11.30-12.10",d1,'subjectView16','teacherView16');
+//        getTimeSlot("12.10-12.50",d1,'subjectView17','teacherView17');
+//        getTimeSlot("12.50-13.30",d1,'subjectView18','teacherView18');
+//        
+//        var d2="tuesday";
+//        getTimeSlot("07.50-08.30",d2,'subjectView2','teacherView2');//subjectVie
+//        getTimeSlot("08.30-09.10",d2,'subjectView22','teacherView22');
+//        getTimeSlot("09.10-09.50",d2,'subjectView23','teacherView23');
+//        getTimeSlot("09.50-10.30",d2,'subjectView24','teacherView24');
+//        getTimeSlot("10.50-11.30",d2,'subjectView25','teacherView25');
+//        getTimeSlot("11.30-12.10",d2,'subjectView26','teacherView26');
+//        getTimeSlot("12.10-12.50",d2,'subjectView27','teacherView27');
+//        getTimeSlot("12.50-13.30",d2,'subjectView28','teacherView28');
+//        
+//        var d3="wednesday";
+//        getTimeSlot("07.50-08.30",d3,'subjectView3','teacherView3');//subjectVie
+//        getTimeSlot("08.30-09.10",d3,'subjectView32','teacherView32');
+//        getTimeSlot("09.10-09.50",d3,'subjectView33','teacherView33');
+//        getTimeSlot("09.50-10.30",d3,'subjectView34','teacherView34');
+//        getTimeSlot("10.50-11.30",d3,'subjectView35','teacherView35');
+//        getTimeSlot("11.30-12.10",d3,'subjectView36','teacherView36');
+//        getTimeSlot("12.10-12.50",d3,'subjectView37','teacherView37');
+//        getTimeSlot("12.50-13.30",d3,'subjectView38','teacherView38');
+//        
+//        var d4="thursday";
+//        getTimeSlot("07.50-08.30",d4,'subjectView4','teacherView4');//subjectVie
+//        getTimeSlot("08.30-09.10",d4,'subjectView42','teacherView42');
+//        getTimeSlot("09.10-09.50",d4,'subjectView43','teacherView43');
+//        getTimeSlot("09.50-10.30",d4,'subjectView44','teacherView44');
+//        getTimeSlot("10.50-11.30",d4,'subjectView45','teacherView45');
+//        getTimeSlot("11.30-12.10",d4,'subjectView46','teacherView46');
+//        getTimeSlot("12.10-12.50",d4,'subjectView47','teacherView47');
+//        getTimeSlot("12.50-13.30",d4,'subjectView48','teacherView48');
+//        
+//        var d5="friday";
+//        getTimeSlot("07.50-08.30",d5,'subjectView5','teacherView5');//subjectVie
+//        getTimeSlot("08.30-09.10",d5,'subjectView52','teacherView52');
+//        getTimeSlot("09.10-09.50",d5,'subjectView53','teacherView53');
+//        getTimeSlot("09.50-10.30",d5,'subjectView54','teacherView54');
+//        getTimeSlot("10.50-11.30",d5,'subjectView55','teacherView55');
+//        getTimeSlot("11.30-12.10",d5,'subjectView56','teacherView56');
+//        getTimeSlot("12.10-12.50",d5,'subjectView57','teacherView57');
+//        getTimeSlot("12.50-13.30",d5,'subjectView58','teacherView58');
+//        
         
     }
     
