6b070eaf03060e143f2f6c06fcea5e6c42e15746
Dinuka Kasun Medis <dinuka-kasun-medis@users.noreply.github.com>
2019-01-07 16:17:06 +0530

    student

--- School system/pages/studentforms/myprogress/viewprogress.php
+++ School system/pages/studentforms/myprogress/viewprogress.php
@@ -2,7 +2,17 @@
 <?php include_once('../studentcommon/header.php'); ?>
 <?php include_once('../studentcommon/sidebar.php'); ?>
 <?php include_once('../studentcommon/script.php'); ?>
-
+<head>
+    <script src="https://www.gstatic.com/firebasejs/5.5.5/firebase-app.js"></script>
+    <script src="https://www.gstatic.com/firebasejs/5.5.5/firebase-firestore.js"></script>
+    <script src="https://www.gstatic.com/firebasejs/5.5.5/firebase-auth.js"></script>
+    <script src="https://www.gstatic.com/firebasejs/5.5.5/firebase-database.js"></script>
+    <script src="https://www.gstatic.com/firebasejs/5.5.5/firebase-storage.js"></script>
+    <script src="../../../firebase models/db.js"></script>
+    <script src="../../../firebase models/timeTable.js"></script>
+    <script src="../../../firebase models/admin.js"></script>
+    <script src="../../../firebase models/login.js"></script>
+</head>
 <script src="https://www.gstatic.com/firebasejs/5.5.5/firebase-app.js"></script>
 <script src="https://www.gstatic.com/firebasejs/5.5.5/firebase-firestore.js"></script>
 <script src="https://www.gstatic.com/firebasejs/5.5.5/firebase-auth.js"></script>
@@ -38,28 +48,28 @@
                                 </thead>
                                 <tbody>
                                 <tr>
-                                    <td><a href="pages/examples/invoice.html">Math in class</a></td>
+                                    <td>Maths</td>
                                     <td>2012/02/02</td>
                                     <td>Math</td>
                                     <td>4</td>
                                     <td><span class="label label-warning">57</span></td>
                                 </tr>
                                 <tr>
-                                    <td><a href="pages/examples/invoice.html">Math in class</a></td>
+                                    <td>Sinhala</td>
                                     <td>2012/02/02</td>
                                     <td>Math</td>
                                     <td>4</td>
                                     <td><span class="label label-danger">10</span></td>
                                 </tr>
                                 <tr>
-                                    <td><a href="pages/examples/invoice.html">Math in class</a></td>
+                                    <td>English</td>
                                     <td>2012/02/02</td>
                                     <td>Math</td>
                                     <td>4</td>
                                     <td><span class="label label-info">70</span></td>
                                 </tr>
                                 <tr>
-                                    <td><a href="pages/examples/invoice.html">Math in class</a></td>
+                                    <td>Tamil</td>
                                     <td>2012/02/02</td>
                                     <td>Math</td>
                                     <td>4</td>
@@ -116,88 +126,74 @@
 
 </body>
 
-
-<script>
-
-
-    var studentfNameLbl
-    var studentmNameLbl
-    var studentlNameLbl
-    var studentiNameLbl
-    var studentGenderLbl
-    var studentBdyLbl
-    var studentReligionLbl
-    var studentEmailLbl
-    var studentAdtLbl
-    var studentMediumLbl
-    var studentClassLbl
-    var studentSectionLbl
-    var studentAddressLbl
-    var studentPhoneLbl
-    var parentNameLbl
-    var parentOccupationLbl
-    var parentEmailLbl
-    var parentPhoneLbl
-
-    document.getElementById('studentfNameLbl').innerHTML = studentfNameLbl;
-    document.getElementById('studentmNameLbl').innerHTML = studentmNameLbl;
-    document.getElementById('studentlNameLbl').innerHTML = studentlNameLbl;
-    document.getElementById('studentiNameLbl').innerHTML = studentiNameLbl;
-    document.getElementById('studentGenderLbl').innerHTML = studentGenderLbl;
-    document.getElementById('studentBdyLbl').innerHTML = studentBdyLbl;
-    document.getElementById('studentReligionLbl').innerHTML = studentReligionLbl;
-    document.getElementById('studentEmailLbl').innerHTML = studentEmailLbl;
-    document.getElementById('studentAdtLbl').innerHTML = studentAdtLbl;
-    document.getElementById('studentMediumLbl').innerHTML = studentMediumLbl;
-    document.getElementById('studentClassLbl').innerHTML = studentClassLbl;
-    document.getElementById('studentSectionLbl').innerHTML = studentSectionLbl;
-    document.getElementById('studentAddressLbl').innerHTML = studentAddressLbl;
-    document.getElementById('studentPhoneLbl').innerHTML = studentPhoneLbl;
-    document.getElementById('parentNameLbl').innerHTML = parentNameLbl;
-    document.getElementById('parentOccupationLbl').innerHTML = parentOccupationLbl;
-    document.getElementById('parentEmailLbl').innerHTML = parentEmailLbl;
-    document.getElementById('parentPhoneLbl').innerHTML = parentPhoneLbl;
-    
-</script>
-
 <?php include_once('../studentcommon/footer.php'); ?>
 
 <script>
-var ctx = document.getElementById("myChart").getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ["2014", "2015", "2016", "2017", "2018", "2019"],
-        datasets: [{
-            // label: '# of Votes',
-            data: [65.56, 19.25, 37.25, 54.20, 62.56, 73.56],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
+    function createGraph(dataset){
+        var ctx = document.getElementById("myChart").getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ["2014", "2015", "2016", "2017", "2018", "2019"],
+                datasets: [{
+                    // label: '# of Votes',
+                    //data: [65.56, 19.25, 37.25, 54.20, 62.56, 73.56],
+                    data:dataset,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        }
+                    }]
                 }
-            }]
-        }
+            }
+        });
     }
-});
+</script>
+
+<script>
+    var School ="abc";
+    getreports("11","6","sinhala","11","1");
+    function getreports(Regno,Class,Grade,Year,Term) {
+        var output = {};
+        db.collection("reports").where("school", "==",School ).where("regno", "==", Regno).where("class", "==", Class).where("grade", "==", Grade).where("year", "==", Year).where("termno", "==", Term)
+            .get()
+            .then(function (querySnapshot) {
+                querySnapshot.forEach(function (doc) {
+                    output = doc.data();
+                    var dataset = [output.sub1,output.sub2,output.sub3,
+                                    output.sub4,output.sub5,output.sub6,
+                                    output.sub7,output.sub8,output.sub9];
+                    createGraph(dataset);
+
+                    //console.log(doc.id, " => ", doc.data());
+
+                });
+            })
+            .catch(function (error) {
+                console.log("Error getting documents: ", error);
+            });
+        return output;
+
+    }
 </script>
